988e5edbc83c27752dd3f1ccdcdaf1399123615a
ashish vilas more <ashishmore@ashishs-MacBook-Air.local>
2023-09-11 14:38:24 +0530

    mac -11/sept/23

---
+++ Strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // there are 2 ways to defining strings  
+    // 1) using string datatype 
+    // 2) using array of char datatype
+
+    string my_str = "This is a sample string";
+    cout <<my_str;
+
+    char name[6] ={'A', 's','h','i','s','h'};
+    cout <<endl<<name;
+
+
+
+}
